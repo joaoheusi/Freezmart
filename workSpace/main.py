9096a03c8ef61e45d69d84c9147915bd68cdae80
Joao Marcelo Heusi <jmarcelo.heusi@gmail.com>
2020-12-15 22:11:21 -0300

    complete

---
+++ workSpace/main.py
@@ -0,0 +1,34 @@
+def web_page():
+    if led.value() == 1:
+        gpio_state="ON"
+    else:
+        gpio_state="OFF"
+
+    html = """<html> <head> <title>Smart Lamp</title> <meta name="viewport" content="width=device-width, initial-scale=1" /> <link rel="icon" href="data:," /> <link rel="stylesheet" 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/v4-shims.css"> <style> 
+     mainbody { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center; } .button { display: inline-block; border: none; border-radius: 4px; color: white; padding: 
+     16px 40px; text-decoration: none; font-size: 30px; margin: 4px; cursor: pointer; } p { font-size: 1.5rem; } </style> </head> <body> <div class="container"> <div class="jumbotron"> <h1>Smart 
+     Lamp &nbsp <a href="/"><span><i class="fa fa-refresh" aria-hidden="true"></i></span></a></h1> </div> <div class="mainbody"> <p>Estado da Led <strong> """ + gpio_state + """</strong></p> <p> 
+     <a href="/?led=on" ><button class="button btn btn-success btn-lg btn-lg"> Ligar </button></a > </p> <p> <a href="/?led=off" ><button class="button btn btn-danger btn-lg btn-lg"> Desligar 
+     </button></a > </p> </div> </div> </body></html>"""
+    return html
+
+  
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+from machine import Pin, ADC 
+from time import sleep
+
+verify click()
+
+
+while True:
+
+   
+    #Sensor de temperatura.
+    adc = ADC(Pin(32))
+    adc.atten(ADC.ATTN_11DB)
+    adc.width(ADC.WIDTH_12BIT)
---
+++ workSpace/webapp.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+from logic import write_json,read_json,get_latest_status,get_current_temperature
+from datetime import datetime
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return  render_template('index.html')
+
+@app.route('/index')
+def index():
+    return  render_template('index.html')
+
+
+@app.route('/aplicacao', methods=['GET','POST'])
+def main_app():
+    if request.method == 'POST':
+        estado = get_latest_status()
+        write_info = estado.get("write_info") 
+        write_json(write_info)
+        estado = get_latest_status()
+        context = estado
+        button_name = estado.get("button_name")
+        button_class = estado.get("button_class")
+        button_url = "main_app"
+        temperature = estado.get("temp")
+
+        return render_template('aplicacao.html',current_temperature=temperature, button_class = button_class, button_name = button_name, button_url =button_url)
+     
+    if request.method == 'GET':
+        estado = get_latest_status()
+        context = estado
+        button_name = estado.get("button_name")
+        button_class = estado.get("button_class")
+        button_url = "main_app"
+        temperature = estado.get("temp")
+
+        return render_template('aplicacao.html', button_class = button_class, button_name = button_name, button_url =button_url, current_temperature = temperature)
+
+@app.route('/historico')
+def history():
+    context = read_json()
+    return  render_template('historico.html', context = context)
+
+@app.route('/paginateste')
+def teste():
+    return  render_template('paginateste.html')
+
+
